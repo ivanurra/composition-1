663e871e6bb02d78787ff0edb449edb704a2db81
Ivan Urra <ivan.urra.wd@gmail.com>
2021-06-23 16:56:06 +0200

    hooks

---
+++ src/views/Contador.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Contador: {{contador}}</h1>
+  <Btn :textoBoton="'Aumentar'" @accion="aumentar" />
+  <Btn :textoBoton="'Disminuir'" @accion="disminuir" />
+</template>
+
+<script>
+import {useContador} from '../hooks/useContador'
+import Btn from '../components/Btn.vue'
+
+export default {
+    components: {
+    Btn
+    },
+    setup(){
+        return {...useContador()}
+    }
+}
+</script>
